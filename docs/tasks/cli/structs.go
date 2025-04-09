dbaa4fad9a8be5015972434d5fd6e2c3f1abe4ca
sujer-ux <67526489+sujer-ux@users.noreply.github.com>
2025-04-09 17:50:26 +0500

    Update structs.go

---
+++ docs/tasks/cli/structs.go
@@ -0,0 +1,14 @@
+type Action struct {
+    Handler     func(data ...string) error
+    NeedsData   bool                       
+    Usage       string                    
+    Description string  
+}
+
+type Command struct {
+    Description string             
+    Actions     map[string]Action
+    Default     *Action
+}
+
+var commands map[string]Command
