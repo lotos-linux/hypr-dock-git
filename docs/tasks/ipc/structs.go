7ac598de1868aebf53194216e9dd0ef2c35cd577
sujer-ux <67526489+sujer-ux@users.noreply.github.com>
2025-04-09 22:12:19 +0500

    Update structs.go

--- docs/tasks/ipc/structs.go
+++ docs/tasks/ipc/structs.go
@@ -1,1 +1,14 @@
-test124777
+type Action struct {
+    Handler     func(data ...string) error
+    NeedsData   bool                       
+    Usage       string                    
+    Description string  
+}
+
+type Command struct {
+    Description string             
+    Actions     map[string]Action
+    Default     *Action
+}
+
+var commands map[string]Command
